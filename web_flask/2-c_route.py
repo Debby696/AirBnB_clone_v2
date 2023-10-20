3500359586dcbf64c40fca4e91c0f10078b1534f
Debby696 <idowuopeyemi696@gmail.com>
2023-10-20 04:34:31 -0700

    a script that starts a Flask web application with variable in the route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -24,7 +24,7 @@ def hbnb():
 @app.route("/c/<text>", strict_slashes=False)
 def cText(text):
     """display “C ” followed by the value of the text variable"""
-    return "c {}".format(text.replace("_", " "))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
