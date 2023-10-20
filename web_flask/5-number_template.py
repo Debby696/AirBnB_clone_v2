9cfea52d5e8659bb06703c35a5dd1f37034a43e7
Debby696 <idowuopeyemi696@gmail.com>
2023-10-20 06:22:05 -0700

    a script that starts a Flask web application and renders a html page

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,8 +4,7 @@ a script that starts a Flask web application
 web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask("__name__")
 
 
