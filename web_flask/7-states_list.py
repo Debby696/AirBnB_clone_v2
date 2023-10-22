eb6b6f82401ec57d3d8597c15be48d28d24e7ecd
Debby696 <idowuopeyemi696@gmail.com>
2023-10-22 16:06:47 -0700

    web flask to run app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
-from flask import Flask
-from flask import render_template
-from models import storage
-from models.state import State
+"""Flask Application Module"""
 
+from flask import Flask
+from models import storage
+from flask import render_template
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays list of states in an HTML page"""
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exception):
-    """this is to remove current SQLAlchemy Session"""
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
